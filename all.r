ce7c1ceac80ee21131960f887bbcdc9bf8b95327
RoshanAntony2004 <roshanantonyprabhakar@gmail.com>
2025-03-13 09:09:04 +0530

    all.r

--- all.r
+++ all.r
@@ -83,3 +83,87 @@ cat("\nFail to reject the null hypothesis. There is no significant association b
 } 
 
 ___________________________________________________________________________________________________________________
+
+5. PROGRAM:
+
+method_A<-c(80,85,88,92,95)
+method_B<-c(78,75,82,87,90)
+method_C<-c(70,72,75,80,85)
+data<-data.frame(
+method=factor(rep(c("A","B","C"),each=5)),
+score=c(method_A,method_B,method_C)
+)
+str(data)
+anova_result<-aov(score~method,data=data)
+summary(anova_result)
+tukey_result<-TukeyHSD(anova_result)
+print(tukey_result)
+
+________________________________________________________________________________________________________________
+
+6. PROGRAM:
+
+library(stats)
+group1<-c(12,14,16,18,20)
+group2<-c(11,13,15,17,19)
+sign_test<-binom.test(sum(group1>group2),length(group1),p=0.5,alternative="greater")
+cat("Sign Test:\n")
+print(sign_test)
+signed_rank_test<-wilcox.test(group1,group2,paired=TRUE,exact=FALSE)
+cat("\n Signed Rank Test (Wilcoxon Signed Rank Test for Paired Samples:\n")
+print(signed_rank_test)
+mw_w_test<-wilcox.test(group1,group2)
+cat("\nMan-Whitney-Wilcoxon Test for Two Samples:\n")
+print(mw_w_test)
+
+______________________________________________________________________________________________________________
+
+7. PROGRAM:
+
+install.packages("forecast")
+library(forecast)
+data<-read.csv("file path")
+data$date<-as.Date(data$date)
+plot(data$date,data$min_temperature,type="l",xlab="Date",ylab="Minimum Temperature",main="Time Series Plot")
+arima_model<-auto.arima(data$min_temperature)
+summary(arima_model)
+future_forecast<-forecast(arima_model,h=30)
+plot(future_forecast,min="Forecasted Minimum Temperature")
+
+___________________________________________________________________________________________________________________
+
+8. PROGRAM:
+
+car_age<-c(4,4,5,5,7,7,8,9,10,11,12)
+price <- c(6300,5800,5700,4500,4500,4200,4200,3100,2100,2500,2200)
+data <- data.frame(x = car_age, y = price)
+linear_model <- lm(y ~ x, data = data)
+summary(linear_model)
+plot(data$x, data$y, main = "Simple Linear Regression", xlab = "X", ylab = "Y")
+abline(linear_model, col = "red")
+new_car_age <- c(6, 8, 10)
+new_data <- data.frame(x = new_car_age)
+predictions <- predict(linear_model, newdata = new_data)
+cat("Predictions for new car ages:", predictions, "\n")
+plot(data$x, data$y, main = "Simple Linear Regression with Predictions", xlab = "Car Age", ylab = "Price")
+abline(linear_model, col = "red")
+points(new_data$x, predictions, col = "blue", pch = 16)
+
+_________________________________________________________________________________________________________________
+
+9. PROGRAM:
+
+library(ggplot2)
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 5, 3, 8, 7)
+model <- lm(y ~ x)
+residuals <- resid(model)
+ggplot(data.frame(Fitted = fitted(model), Residuals = residuals), aes(x = Fitted, y = Residuals)) +
+  geom_point() +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  labs(title = "Residuals vs Fitted Values", x = "Fitted Values", y = "Residuals")
+shapiro_test <- shapiro.test(residuals)
+qqnorm(residuals)
+qqline(residuals)
+
+__________________________________________________________________________________________________________________
